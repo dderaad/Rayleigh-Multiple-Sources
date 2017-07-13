e1a579b2136feed98bdf06444217384aa3a82271
dderaad <dderaad@uw.edu>
2017-07-13 12:48:38 -0700

    Update Rayleigh_for_Multiple_Sources.m

--- Rayleigh_for_Multiple_Sources.m
+++ Rayleigh_for_Multiple_Sources.m
@@ -37,7 +37,7 @@ R = 1e-2; % radius of curvature (m)
 radius = 1e-3; % radius of aperture (m)
 
 R = 44.5e-3; 
-radius = 44.5e-3;
+radius = 44.5e-3 / 2;
 
 %% the Rayleigh length
 % 
@@ -56,31 +56,36 @@ RL = S/lambda; % in meters
 
 % number proportional to complexity of sphere (detail below)
 % means higher numbers are required for smaller apertures
-v_n = 500;
+v_n = 200;
 [xs, ys, zs] = sphere(v_n); % efficiency of this is ~O(n^2)
 % can use function ellipsoid when geometry becomes more complicated
 
+% Apply curvature radius
 xs = xs(:) * R;
 ys = ys(:) * R;
 zs = zs(:) * R;
 
+% Bowl
 mask1 = (zs < 0);
 xs = xs(mask1);
 ys = ys(mask1);
 zs = zs(mask1);
 
+% Aperture
 mask2 = (xs.^2 + ys.^2 <= radius^2);
 xs = xs(mask2);
 ys = ys(mask2);
 zs = zs(mask2);
 
-zs = zs - R;
+% Cuts
+% Polynomials above the hemisphere
+% mask3 = 
 
 %% setup
-%theta = 0; % angle about x axis. 
+theta = 0; % angle about x axis. 
 phi = 0;   % angle about y axis
-%gamma = 0; % angle about z axis
-%Rx = @(t)[1 0 0; 0 cos(t) -sin(t); 0 sin(t) cos(t)]; % x rotation matrix for angle t
+gamma = 0; % angle about z axis
+Rx = @(t)[1 0 0; 0 cos(t) -sin(t); 0 sin(t) cos(t)]; % x rotation matrix for angle t
 Ry = @(p)[cos(p) 0 sin(p); 0 1 0; -sin(p) 0 cos(p)]; % y ''
 Rz = @(g)[cos(g) -sin(g) 0; sin(g) cos(g) 0; 0 0 1]; % z ''
 
@@ -103,8 +108,6 @@ if phi ~= 0
     end
 end
 
-
-
 %{
 % Display S
 scatter3(xs, ys, zs)
@@ -113,15 +116,14 @@ scatter3(xs, ys, zs)
 figure
 %}
 
-
+%
 %% field points
 
-dxf = lambda / 10;
-delta = .25e-2;
-zf = -.5 * R:dxf: .5 * R;
-%zf = -(25e-4)/2-.0445:2e-5:(25e-4)/2-.0445;
-xf = [-radius*.5:dxf:radius*.5];
-%xf = [-10e-4:2e-5:10e-4];
+dzf = lambda / 10 * 2;
+dxf = dzf * radius/R;
+delta = .5;
+zf = [-delta*R:dzf:-dzf 0:dzf:R*delta];
+xf = [delta*-radius:dxf:-dxf 0:dxf:delta*radius];
 yf = xf;
 yf=0;
 
@@ -132,57 +134,54 @@ yf=0;
 % xz at y
 % volume of points
 
-%% calculate axial pressure
+%% Alternate Method
+%
+A = sqrt(-1) .* w .* rho .* c .* u/ (2 .* pi);
+deltaS = dxf .^ 2;
+R = @(x, y, z)sqrt((x-xs).^2 + (y-ys).^2 + (z-zs).^2);
+pXYZ = @(x, y, z)sum(exp(-sqrt(-1).*k.*R(x, y, z))./R(x, y, z));
 
-A = ( sqrt(-1).*k.*rho.*c.*u ) ./ ( 2.*pi );
+pf = zeros([length(xf), length(zf)]);
 
-for n = 1:length(zf);
-    R = sqrt(xs.^2 + ys.^2 + zf(n).^2);
-    pAx(n) = sum(sum( exp(-1.*sqrt(-1).*k.*R ) ./ R ));
-end; %z
-
-pAx = pAx.*A.*dxf.*dxf;
-
-subplot(2, 1, 1);
-plot(zf*1000,abs(pAx)/sqrt(max(pAx)*max(pAx)'));
-grid minor
-drawnow;
-
-
-for n = 1:length(xf);
-    R = sqrt(xf(n).^2 + ys.^2 + zs.^2);
-    pAz(n) = sum(sum( exp(-1.*sqrt(-1).*k.*R ) ./ R ));
-end; %z
-
-subplot(2, 1, 2);
-pAz = pAz.*A.*dxf.*dxf;
-plot(xf*1000,abs(pAz)/sqrt(max(pAz)*max(pAz)'));
-drawnow;
-
-a_max = norm(pAx, inf);
-ca_max = norm(pAz, inf);
-
-
-%{
-%% calculate transverse field at focus
-A = ( sqrt(-1).*k.*rho.*c.*u ) ./ ( 2.*pi );
-
-
-% ~O(n^2); some for loops may be nearly empty
 for n = 1:length(xf)
-    for m = 1:length(yf)
-        for l = 1:length(zf)
-    
-        R = sqrt((xf(n)-xs).^2 + (ys).^2 + (zf(l)-zs).^2 );
-        pXYZ(n,m,l) = sum(sum( exp(-1.*sqrt(-1).*k.*R ) ./ R ));
-        
-        end %z
-    end %y    
-end %x
+    for m = 1:length(zf)
+        pf(m,n) = pXYZ(xf(n), 0, zf(m));
+    end
+    if mod(n, 10) == 0
+        sprintf('%.0f %s', 100*n / length(xf), '%')
+    end
+end
 
-pXYZ = pXYZ.*A.*dxf.*dxf;
+pf = A .* deltaS .* pf;
 
-pXZ = squeeze(pXYZ);
-imagesc(1000*zf,1000*xf,abs(pXZ))
-
-%}
+%subplot(3,1,1)
+waterfall(1000*xf,1000*zf,abs(pf)/1e3)
+xlabel('x (mm)')
+ylabel('z (mm)')
+zlabel('P (Kp)')
+hold on 
+scatter3(1000*xs, 1000*zs, 1000*ys)
+title('Pressure Field, Focus at 0')
+%subplot(3,1,2)
+%imagesc(1000*zf,1000*xf,real(pf))
+%subplot(3,1,3)
+%imagesc(1000*zf,1000*xf,imag(pf))
+figure
+subplot(2,1,1)
+pCAf = pf(round(length(pf)/2), :);
+pAf = pf(:, round(length(pf)/2));
+plot(1000*xf, real(pAf))
+hold on
+plot(1000*xf, imag(pAf))
+hold on
+plot(1000*xf, abs(pAf), 'k')
+[~, I] = max(abs(pCAf));
+title(sprintf('Axial Pressure | Max: %f mm', 1000*zf(I)))
+subplot(2,1,2)
+plot(1000*zf, real(pCAf))
+hold on
+plot(1000*zf, imag(pCAf))
+hold on
+plot(1000*zf, abs(pCAf), 'k')
+[~, I] = max(abs(pCAf));
+title(sprintf('Cross-Axial Pressure | Max: %f mm', 1000*xf(I)))
